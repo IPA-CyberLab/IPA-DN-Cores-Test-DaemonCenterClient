865c97e912f8f9755b297455d74e96e5117bb7e2
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2019-08-17 22:13:23 +0900

    Hello 15

--- IPA-DN-Cores-Test-DaemonCenterClient/Controllers/HomeController.cs
+++ IPA-DN-Cores-Test-DaemonCenterClient/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace IPA_DN_Cores_Test_DaemonCenterClient.Controllers
         {
             KeyValueList<string, string> o = new KeyValueList<string, string>();
 
-            string msg = "Hello 14!";
+            string msg = "Hello 15!";
 
             o.Add("メッセージ", msg);
 
